9d51cf2115fc2bfa5cae611936390809f0e5ae19
nilsec <nils.eckstein@googlemail.com>
2019-07-24 18:35:13 -0400

    Add setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+        name='funlib.learn.torch',
+        version='0.1',
+        url='https://github.com/funkelab/funlib.learn.torch',
+        author='Funkelab',
+        author_email='funkej@janelia.hhmi.org',
+        license='MIT',
+        packages=[
+            'funlib.learn.torch',
+            'funlib.learn.torch.models',
+            'funlib.learn.torch.misc',
+        ]
+)
