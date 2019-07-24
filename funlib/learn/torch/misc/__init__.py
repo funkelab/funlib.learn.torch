dce1a2b5e5228ae9a155580476d29b8163517b06
nilsec <nils.eckstein@googlemail.com>
2019-07-24 18:36:22 -0400

    Add vgg and integrated gradients

---
+++ funlib/learn/torch/misc/__init__.py
@@ -0,0 +1,3 @@
+from __future__ import absolute_import
+
+from .integrated_gradients import get_integrated_gradients # noqa
---
+++ funlib/learn/torch/models/__init__.py
@@ -0,0 +1,3 @@
+from __future__ import absolute_import
+
+from .vgg3d import Vgg3D # noqa
