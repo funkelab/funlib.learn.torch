290d00aa81fda22960abfc1d9e0b765d73ea6ac1
nilsec <nils.eckstein@googlemail.com>
2020-10-09 06:52:09 -0400

    Add minimal test

---
+++ funlib/tests/test_vgg3d.py
@@ -0,0 +1,23 @@
+from funlib.learn.torch import models
+import numpy as np
+import pytest
+import torch
+import unittest
+import warnings
+warnings.filterwarnings("error")
+
+class TestVgg3D(unittest.TestCase):
+    def test_creation(self):
+        vgg = models.Vgg3D(
+            input_size=(128,128,128))
+
+        x = np.zeros((1,128,128,128), dtype=np.float64)
+        x = torch.from_numpy(x).float()
+
+        y = vgg.forward(x).data.numpy()
+        print(y.shape)
+
+        assert y.shape == (1, 6)
+
+if __name__ == "__main__":
+    unittest.main()
