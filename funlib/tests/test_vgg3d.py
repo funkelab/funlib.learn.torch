150886b2af79525a0cca776a610768438b9b2a24
nilsec <nils.eckstein@googlemail.com>
2020-10-09 08:51:40 -0400

    Add simple (non-automated) test for architecture change

--- funlib/tests/test_vgg3d.py
+++ funlib/tests/test_vgg3d.py
@@ -19,5 +19,32 @@ class TestVgg3D(unittest.TestCase):
 
         assert y.shape == (1, 6)
 
+
+        vgg = models.Vgg3D(
+            input_size=(128,128,128),
+            fmap_inc=(4,1,1,1))
+
+        x = np.zeros((1,128,128,128), dtype=np.float64)
+        x = torch.from_numpy(x).float()
+
+        y = vgg.forward(x).data.numpy()
+        print(y.shape)
+
+        assert y.shape == (1, 6)
+
+        vgg = models.Vgg3D(
+            input_size=(128,128,128),
+            fmap_inc=(4,1,1,1),
+            n_convolutions=(4,2,2,2))
+
+        x = np.zeros((1,128,128,128), dtype=np.float64)
+        x = torch.from_numpy(x).float()
+
+        y = vgg.forward(x).data.numpy()
+        print(y.shape)
+
+        assert y.shape == (1, 6)
+
+
 if __name__ == "__main__":
     unittest.main()
