1c296ab1ac445ab7243cf4967224f59db6890c43
William Patton <wllmpttn24@gmail.com>
2024-09-12 10:29:02 -0700

    update packaging and pass tests

--- funlib/learn/torch/__init__.py
+++ funlib/learn/torch/__init__.py
@@ -1,5 +1,6 @@
-from __future__ import absolute_import
-
 from . import models, misc
 
 __all__ = ['models', 'misc']
+
+__version__ = "0.1.0"
+__version_info__ = tuple(int(i) for i in __version__.split("."))
--- tests/test_vgg3d.py
+++ tests/test_vgg3d.py
@@ -4,14 +4,15 @@ import pytest
 import torch
 import unittest
 import warnings
+
 warnings.filterwarnings("error")
+
 
 class TestVgg3D(unittest.TestCase):
     def test_creation(self):
-        vgg = models.Vgg3D(
-            input_size=(128,128,128))
+        vgg = models.Vgg3D(input_size=(128, 128, 128))
 
-        x = np.zeros((1,128,128,128), dtype=np.float64)
+        x = np.zeros((1, 1, 128, 128, 128), dtype=np.float64)
         x = torch.from_numpy(x).float()
 
         y = vgg.forward(x).data.numpy()
@@ -19,12 +20,9 @@ class TestVgg3D(unittest.TestCase):
 
         assert y.shape == (1, 6)
 
+        vgg = models.Vgg3D(input_size=(128, 128, 128), fmap_inc=(4, 1, 1, 1))
 
-        vgg = models.Vgg3D(
-            input_size=(128,128,128),
-            fmap_inc=(4,1,1,1))
-
-        x = np.zeros((1,128,128,128), dtype=np.float64)
+        x = np.zeros((1, 1, 128, 128, 128), dtype=np.float64)
         x = torch.from_numpy(x).float()
 
         y = vgg.forward(x).data.numpy()
@@ -33,11 +31,12 @@ class TestVgg3D(unittest.TestCase):
         assert y.shape == (1, 6)
 
         vgg = models.Vgg3D(
-            input_size=(128,128,128),
-            fmap_inc=(4,1,1,1),
-            n_convolutions=(4,2,2,2))
+            input_size=(128, 128, 128),
+            fmap_inc=(4, 1, 1, 1),
+            n_convolutions=(4, 2, 2, 2),
+        )
 
-        x = np.zeros((1,128,128,128), dtype=np.float64)
+        x = np.zeros((1, 1, 128, 128, 128), dtype=np.float64)
         x = torch.from_numpy(x).float()
 
         y = vgg.forward(x).data.numpy()
